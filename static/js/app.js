0583bc5ca48891a358ec29afbdf6cd66d6ec4098
Noah LeVoir <noahlevoir@Noahs-MacBook-Pro.local>
2021-04-23 15:58:54 -0500

    Implemented code to draw bargraph - code from office hours

--- static/js/app.js
+++ static/js/app.js
@@ -6,6 +6,40 @@ console.log("app.js loaded");
 // Functions to draw each graph per homework instructions
 function drawBargraph(sampleID) {
     console.log(`Draw Bargraph for ${sampleID}`);
+
+    d3.json("samples.json").then(data => {
+        // console.log(data)
+
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == sampleID);
+        // console.log(resultArray);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
+        var barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            type: "bar",
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: "h"
+        }
+
+        var barArray = [barData];
+
+        var barLayout = {
+            title: "Top 10 Bacterial Cultures Found",
+            margin: {t:30, l:150}
+        }
+
+        Plotly.newPlot(bar, barArray, barLayout)
+
+    });
+
 }
 
 function drawBubblechart(sampleID) {
